dae648259605f1f1040bf76b8395acc3a0cf5377
md_rajib_rari <rajibhossen688@gmail.com>
2023-04-01 01:29:51 +0600

    01-04-2023 working

--- BloodBank/Controllers/HomeController.cs
+++ BloodBank/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BloodBank.Models;
 
 namespace BloodBank.Controllers
 {
     public class HomeController : Controller
     {
+        Blood_BankFEntities db = new Blood_BankFEntities();
         public ActionResult Index()
         {
+            var bloodGroup = db.BloodGroups.ToList();
+            ViewBag.bldgrup = bloodGroup;
+
+
+
+
             return View();
         }
 
