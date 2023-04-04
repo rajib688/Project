a86d8582dc306fd57b0493e7e0ca655a59156fbf
md_rajib_rari <rajibhossen688@gmail.com>
2023-04-04 17:36:17 +0600

    add mofity project

---
+++ BloodBank/Controllers/Blood_RequestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BloodBank.Models;
+
+namespace BloodBank.Controllers
+{
+    [Authorize]
+    public class Blood_RequestController : Controller
+    {
+        Blood_BankFEntities db = new Blood_BankFEntities();
+        // GET: Blood_Request
+        public ActionResult Index()
+        {
+            var bloodGroup = db.BloodGroups.ToList();
+            ViewBag.bldgrup = bloodGroup;
+
+
+
+            return View();
+        }
+    }
+}
